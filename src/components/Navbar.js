d5b432120c3ebf3fb8c941dff0415dd2688e3fec
lucassherwin <lksherwin@gmail.com>
2021-02-15 16:34:22 -0500

    done with navbar styling for now

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,19 +3,11 @@ import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
 export default function Navbar() {
-  const link = {
-    width: '100px',
-    adding: '12px',
-    margin: '0 6px 6px',
-    background: 'blue',
-    textDecoration: 'none',
-    color: 'white',
-  }
-
   const NavContainer = styled.div`
     display: flex;
     flex-direction: row;
     align-items: center;
+    margin-left: 2vw;
   `;
 
   const Nav = styled(NavLink)`
@@ -24,12 +16,9 @@ export default function Navbar() {
     color: 'grey';
   `;
 
-  // TODO: Remove colors from links
-  // They should be black when active gray otherwise
-
 return (
     <NavContainer>
-      <Nav style={{fontSize: 40}} activeStyle={{color: 'black'}} to="/" exact style={{color: 'black'}}><p>HOUSANDARTS</p></Nav>
+      <Nav activeStyle={{color: 'black'}} to="/" exact style={{color: 'black', fontSize: 40}}><p>HOUSANDARTS</p></Nav>
       <Nav to='/posters' exact activeStyle={{color: 'black'}} style={{color: 'gray'}}><p >Posters</p></Nav>
       <Nav to='/patterns' exact activeStyle={{color: 'black'}} style={{color: 'gray'}}><p>Patterns</p></Nav>
       <Nav to='/apparel' exact activeStyle={{color: 'black'}} style={{color: 'gray'}}><p>Apparel</p></Nav>
