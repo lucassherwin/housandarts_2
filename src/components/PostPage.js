9a37199051557304012d4ddf6f7f30a59bfd1c43
lucassherwin <lksherwin@gmail.com>
2021-02-16 14:45:56 -0500

    updated PostPage

--- src/components/PostPage.js
+++ src/components/PostPage.js
@@ -4,6 +4,7 @@ export default function PostPage({ currentPost }) {
     return (
         <div>
             <h1>{currentPost.title}</h1>
+            <img src={currentPost.src} alt={currentPost.title} height='200px' />
         </div>
     )
 }
